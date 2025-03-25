4a0585fab3e91d821eda9be8787c15f40b8b7832
Evgeny Feigelman <devops.evgeny@gmail.com>
2025-03-25 22:55:43 +0200

    aa

--- src/app/app.py
+++ src/app/app.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/<name>')
+def hello(name):
+    return f'Hello {name}'
+
+@app.route('/check_file/<file_name>')
+def check_file(file_name):
+    try:
+        with open(file_name) as FILE:
+            data = FILE.readlines()
+        return data
+    except FileNotFoundError:
+        return f'[!] No such file {file_name} found'
+
+
+app.run(host='127.0.0.1', port=8000, debug=True)
