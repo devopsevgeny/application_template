40c1add29903689a6f91c887f90a1679accec402
Evgeny Feigelman <namlegief@gmail.com>
2025-03-28 21:09:46 +0300

    changes to the code

--- install_py.sh
+++ install_py.sh
@@ -11,11 +11,10 @@ set -o errexit
 set -o pipefail 
 set -x
 
-read -p "what is the name of your project" PROJECT_NAME
-WORK_DIR="~/projects/$PROJECT_NAME"
-PACKAGES=("python3" "python3-pip" "pipx" "python3-venv"  "makeself" "sqlite3") 
+WORK_DIR="$HOME/projects/$PROJECT_NAME"
+# PACKAGES=("python3" "python3-pip" "pipx" "python3-venv"  "makeself" "sqlite3") 
 
-# Function to check for root privileges
+
 check_no_root() {
     if [[ $EUID == "0" ]] || [[ $UID == "0" ]]; then
         echo "Do not use root user or sudo "
@@ -27,21 +26,28 @@ check_no_root() {
 }
 
 # Checking which OS
-whichOS=$(cat /etc/os-release | grep "ID_LIKE=" | sed 's/ID_LIKE=//')
-echo "Detected OS: $whichOS"
+# whichOS=$(cat /etc/os-release | grep "ID_LIKE=" | sed 's/ID_LIKE=//')
+if [[ ! -f /etc/os-release ]]; then
+    echo "Error: /etc/os-release not found. Cannot detect operating system."
+    exit 1
+else
+    . /etc/os-release
+    whichOS=$(echo "$ID_LIKE" | tr '[:upper:]' '[:lower:]')
+    echo "Detected OS: $whichOS
+fi
+
+"
 
 function install_debian(){
 	echo Installing pakages for $whichOS OS.
-	sudo apt update;sudo apt upgrade -y;sudo apt install -y "${PACKAGES[@]}"
+	sudo apt update;sudo apt upgrade -y;sudo apt install -y python3 python3-pip pipx python3-venv makeself sqlite3
 }
 function install_rhel(){
 	sudo dnf update -y;
 	sudo dnf install -y epel-release || sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm;
 	sudo dnf update -y;
-	sudo dnf install -y  "${PACKAGES[@]}"
-	echo Installing pakages for $whichOS OS.
-    
-        
+	sudo dnf install -y  python3 python3-pip python3-virtualenv makeself sqlite
+	echo Installing pakages for $whichOS OS.     
     
 }
 
@@ -50,3 +56,16 @@ case $whichOS in
     *fedora*) install_rhel ;;
     *) echo "$whichOS is not supported" && exit 1 ;;
 esac
+
+# Function to read user's input to create a new project.
+
+function create_project_dir(){
+    read -p "what is the name of your project" PROJECT_NAME
+    if [[ ! $WORK_DIR/$PROJECT_NAME ]]; then 
+        echo "$WORK_DIR/$PROJECT_NAME not exists"
+    else
+         echo "$WORK_DIR/$PROJECT_NAME already exists"
+    fi
+}
+
+create_project_dir
